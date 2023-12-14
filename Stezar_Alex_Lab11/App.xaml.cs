08edfff2a057da2a39ee671910e31c0fadb9fb5f
Alex Stezar <alex.stezar07@gmail.com>
2023-12-15 01:43:55 +0200

    Add project files.

---
+++ Stezar_Alex_Lab11/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Stezar_Alex_Lab11.Data;
+
+namespace Stezar_Alex_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Stezar_Alex_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Stezar_Alex_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
